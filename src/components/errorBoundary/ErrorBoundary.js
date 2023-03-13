74128ac29d0810584bed0f50eecc6a6407da00bc
Yevhen Bekir <yevhenbekir@gmail.com>
2023-03-13 21:22:10 +0200

    AppHeader, RandomChar, CharList, CharInfo and ErrorBoundary were created

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class ErrorBoundary extends Component {
+    state={
+        error: false
+    }
+
+    static getDerivedStateFromError(error){                 //Метод, який ТІЛЬКИ оновлює state
+        return ({error: true})                              //Повертає об'єкт, який оновлює state
+    }
+
+    componentDidCatch(err, info){
+        console.log(err, info)
+        this.setState({error: true})
+    }
+
+    render(){
+        if(this.state.error){
+            return <ErrorMessage/>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
